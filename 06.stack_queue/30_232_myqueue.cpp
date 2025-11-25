f95c6a96ddaf637935b6995942ba32a578964346
gomokoro <iamlightart@126.com>
2025-11-25 20:40:49 +0800

    day1 stack_queue

--- 06.stack_queue/30_232_myqueue.cpp
+++ 06.stack_queue/30_232_myqueue.cpp
@@ -11,6 +11,9 @@
 using namespace std;
 class MyQueue
 {
+    stack<int> s1;
+    stack<int> s2;
+
 public:
     MyQueue()
     {
@@ -18,26 +21,43 @@ public:
 
     void push(int x)
     {
+        while (!s2.empty())
+        {
+            s1.push(s2.top());
+            s2.pop();
+        }
+        s1.push(x);
+        while (!s1.empty())
+        {
+            s2.push(s1.top());
+            s1.pop();
+        }
     }
 
     int pop()
     {
+        int result = s2.top();
+        s2.pop();
+        return result;
     }
 
     int peek()
     {
+        return s2.top();
     }
 
     bool empty()
     {
+        return s2.empty();
     }
 };
 
-/**
- * Your MyQueue object will be instantiated and called as such:
- * MyQueue* obj = new MyQueue();
- * obj->push(x);
- * int param_2 = obj->pop();
- * int param_3 = obj->peek();
- * bool param_4 = obj->empty();
- */
+// Your MyQueue object will be instantiated and called as such:
+int main()
+{
+    MyQueue *obj = new MyQueue();
+    obj->push(x);
+    int param_2 = obj->pop();
+    int param_3 = obj->peek();
+    bool param_4 = obj->empty();
+}
