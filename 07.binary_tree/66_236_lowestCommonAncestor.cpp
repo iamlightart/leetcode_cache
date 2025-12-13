90dbc3ec1bf475b73132f42a30ee78fe804974a8
gomokoro <iamlightart@126.com>
2025-12-13 23:10:07 +0800

    12-13

--- 07.binary_tree/66_236_lowestCommonAncestor.cpp
+++ 07.binary_tree/66_236_lowestCommonAncestor.cpp
@@ -9,8 +9,25 @@ struct TreeNode
     TreeNode *right;
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
-// 性能有问题，值传递result会造成大量拷贝，用引用传递和回溯处理比较好。今天头不舒服明天再弄了
+// 标准答案,怎么能简单成这样。。。
+
 class Solution
+{
+public:
+    TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
+    {
+        if (!root || root == p || root == q)
+            return root;
+        TreeNode *left = lowestCommonAncestor(root->left, p, q);
+        TreeNode *right = lowestCommonAncestor(root->right, p, q);
+        if (left && right)
+            return root;            // p,q 分布在左右子树
+        return left ? left : right; // p,q 在同一侧
+    }
+};
+
+// 回溯的方法,复制两次效率略低，已ac
+class MySolution
 {
 public:
     TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
@@ -21,7 +38,54 @@ public:
         int length = min(path_p.size(), path_q.size());
         if (length == 1)
             return path_p[0];
-        for (int i = 0; i < length - 1; i++)
+        for (int i = 0; i < length; i++)
+        {
+            if (path_p[i] != path_q[i])
+                return path_p[i - 1];
+        }
+        return path_p.size() > path_q.size() ? path_q[length - 1] : path_p[length - 1];
+    }
+
+private:
+    vector<TreeNode *> get_path(TreeNode *root, TreeNode *target)
+    {
+        vector<TreeNode *> result{};
+        if (root == nullptr)
+            return result;
+        get_path_impl(root, target, result);
+        return result;
+    };
+    bool get_path_impl(TreeNode *node, TreeNode *target, vector<TreeNode *> &result)
+    {
+        if (!node)
+            return false;
+        result.push_back(node);
+        if (node == target)
+            return true;
+        else
+        {
+            if (get_path_impl(node->left, target, result))
+                return true;
+            if (get_path_impl(node->right, target, result))
+                return true;
+            result.pop_back();
+        }
+        return false;
+    };
+};
+// 性能有问题，值传递result会造成大量拷贝，用引用传递和回溯处理比较好。今天头不舒服明天再弄了
+class OutOfMemorySolution
+{
+public:
+    TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
+    {
+        vector<TreeNode *> path_p = get_path(root, p);
+        vector<TreeNode *> path_q = get_path(root, q);
+        TreeNode *result{};
+        int length = min(path_p.size(), path_q.size());
+        if (length == 1)
+            return path_p[0];
+        for (int i = 0; i < length; i++)
         {
             if (path_p[i] != path_q[i])
                 return path_p[i - 1];
