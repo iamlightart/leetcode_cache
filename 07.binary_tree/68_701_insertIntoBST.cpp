f79c972004e87ba748eb2a6da7d45d11b12f6e4e
gomokoro <iamlightart@126.com>
2025-12-15 22:32:13 +0800

    2025-12-15

--- 07.binary_tree/68_701_insertIntoBST.cpp
+++ 07.binary_tree/68_701_insertIntoBST.cpp
@@ -16,5 +16,25 @@ class Solution
 public:
     TreeNode *insertIntoBST(TreeNode *root, int val)
     {
+        if (!root)
+            root = new TreeNode(val);
+        else
+            insertImpl(root, val);
+        return root;
+    }
+
+private:
+    // 直接用指针做参数也是值传递，想用同一个指针必须要加引用
+    void insertImpl(TreeNode *&root, int val)
+    {
+        if (!root)
+            root = new TreeNode(val);
+        else
+        {
+            if (root->val > val)
+                insertImpl(root->left, val);
+            if (root->val < val)
+                insertImpl(root->right, val);
+        }
     }
 };
