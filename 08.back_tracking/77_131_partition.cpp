35b075131d24d2f0f92cef2ab8872ebbc7f29a5b
gomokoro <iamlightart@126.com>
2025-12-20 22:54:59 +0800

    2025-12-20

--- 08.back_tracking/77_131_partition.cpp
+++ 08.back_tracking/77_131_partition.cpp
@@ -6,5 +6,53 @@ class Solution
 public:
     vector<vector<string>> partition(string s)
     {
+        partitionImpl(s, 0);
+        return results;
     }
+    vector<vector<string>> results;
+    vector<string> result;
+    // string hodor = "";
+    // string hodor_prev = "";
+    void partitionImpl(string &s, int start)
+    {
+        for (int i = start; i < s.size(); i++)
+        {
+            string temp = s.substr(start, i - start + 1);
+            if (isPalindrome(temp))
+            {
+                result.push_back(temp);
+                // hodor_prev = hodor;
+                // hodor += temp;
+                if (i == s.size() - 1)
+                    results.push_back(result);
+                else
+                    partitionImpl(s, i + 1);
+                result.pop_back();
+                // hodor = hodor_prev;
+            }
+        }
+    }
+    // 用左右指针效率更高
+    bool isPalindrome(string &s)
+    {
+        int left = 0, right = s.size() - 1;
+        while (left < right)
+        {
+            if (s[left] != s[right])
+                return false;
+            else
+            {
+                left++;
+                right--;
+            }
+        }
+        return true;
+    }
+    // 反转判断回文数效率略低，但提交没有显示
+    // bool isPalindrome(string &s)
+    // {
+    //     string temp = s;
+    //     reverse(temp.begin(), temp.end());
+    //     return (temp == s) ? true : false;
+    // }
 };
