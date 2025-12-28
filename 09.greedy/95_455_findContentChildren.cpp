7402b220708f3dcd967386e1b4ede80efa606e1b
gomokoro <iamlightart@126.com>
2025-12-28 23:24:33 +0800

    2025-12-28

---
+++ 09.greedy/95_455_findContentChildren.cpp
@@ -0,0 +1,10 @@
+// https://leetcode.cn/problems/assign-cookies
+#include <iostream>
+using namespace std;
+class Solution
+{
+public:
+    int findContentChildren(vector<int> &g, vector<int> &s)
+    {
+        }
+};
