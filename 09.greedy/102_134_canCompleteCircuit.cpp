b4cec445cb2530aa828dbde012493427bea35dd4
gomokoro <iamlightart@126.com>
2025-12-31 21:37:32 +0800

    2025-12-31

--- 09.greedy/102_134_canCompleteCircuit.cpp
+++ 09.greedy/102_134_canCompleteCircuit.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+
 class Solution
 {
 public:
@@ -16,7 +17,86 @@ public:
             return -1;
         else
         {
+            int sum = 0;
+            int start = 0;
+            for (int i = 0; i < rel_gas.size(); i++)
+            {
+                sum += rel_gas[i];
+                if (sum < 0)
+                {
+                    sum = 0;
+                    start = i + 1;
                 }
+            }
+            return start;
+        }
+        return -1;
+    }
+};
+class gptSolution
+{
+public:
+    int canCompleteCircuit(vector<int> &gas, vector<int> &cost)
+    {
+        int total = 0;
+        int curr = 0;
+        int start = 0;
+
+        for (int i = 0; i < gas.size(); i++)
+        {
+            int diff = gas[i] - cost[i];
+            total += diff;
+            curr += diff;
+
+            if (curr < 0)
+            {
+                start = i + 1;
+                curr = 0;
+            }
+        }
+
+        return total >= 0 ? start : -1;
+    }
+};
+// 循环反复卡住，使用gpt提供的答案了
+class WrongLoopSolution
+{
+public:
+    int canCompleteCircuit(vector<int> &gas, vector<int> &cost)
+    {
+        vector<int> rel_gas(gas.size());
+        int has_result = 0;
+        for (int i = 0; i < gas.size(); i++)
+        {
+            rel_gas[i] = gas[i] - cost[i];
+            has_result += rel_gas[i];
+        }
+        if (has_result < 0)
+            return -1;
+        else
+        {
+            int sum = 0;
+            int i = 0;
+            int start = 0;
+            while (true)
+            {
+                sum += rel_gas[i];
+                i++;
+                if (i > rel_gas.size() - 1)
+                    i = 0;
+                if (sum == 0)
+                    return start;
+                if (sum < 0)
+                {
+                    sum = 0;
+                    start = i;
+                }
+                if (sum > 0 && i == start)
+                {
+                    return start;
+                }
+            }
+        }
         return -1;
     }
 };
